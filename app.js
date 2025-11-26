c913cb738f6779f14ba2860650c5f957e1aab39f
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2025-11-26 10:52:26 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"+txtName.value)
-.then(res => res.json())
-.then(data => {
- document.getElementById("")
-});
+let recipe=document.getElementById("txtName").value;
+// let search=document.getElementById("Search");
 
+function getRecipe(){
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=a27be5aa="  + recipe.value)
+    .then(res => res.json())
+    .then(data => {
+       let meal=data.meals[0];
+    document.getElementById("result").innerHTML=`
+        <h2>Name: ${meals[0].strMeal}</h2>
+        <h3>Category: ${meals[0].strCategory}</h3>
+        <h3>Area: ${meals[0].strArea}</h3>
+        <h3>Instructions: ${meals[0].strInstructions}</h3>
+        <img src="${meals[0].strMealThumb}" alt="Meal Image" width="300">
+        <h3>Youtube Link: <a href="${meals.strYoutube}" target="_blank">${data.meals[0].strYoutube}</a></h3>
+        ` ;     
+
+
+})
+}
